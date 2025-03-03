6ff71649dc9135de0350c0e2c8a9abb10d4ee5a7
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2025-03-03 10:26:12 +0300

    create controller for book routes

--- routes/book-routes.js
+++ routes/book-routes.js
@@ -1,23 +1,9 @@
 import express from "express";
-import { Book } from "../models";
+import { getMovie, getMovies } from "../controllers";
 
 const router = express.Router();
 
-const handleError = (response, errorCode, error) => {
-  response.status(errorCode).json({ error });
-};
-
-router.get("/card-sets", (request, response) =>
-  Book.find()
-    .sort({ title: 1 })
-    .then((books) => response.status(200).json(books))
-    .catch((error) => handleError(response, 500, error.message))
-);
-
-router.get("/cards/:id", (request, response) =>
-  Book.findById(request.params.id)
-    .then((book) => response.status(200).json(book))
-    .catch((error) => handleError(response, 500, error.message))
-);
+router.get("/card-sets", getMovies);
+router.get("/cards/:id", getMovie);
 
 export { router as BookRoutes };
